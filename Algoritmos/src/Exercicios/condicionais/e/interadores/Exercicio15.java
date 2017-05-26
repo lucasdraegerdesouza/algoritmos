4d41b0e3f0c689c84eaf6f08308c5a285cb5559d
Noturno <Noturno@192.168.7.26>
2017-05-26 19:30:46 -0300

    Exercicio14 feito

---
+++ Algoritmos/src/Exercicios/condicionais/e/interadores/Exercicio15.java
@@ -0,0 +1,16 @@
+package Exercicios.condicionais.e.interadores;
+
+import java.util.Scanner;
+
+public class Exercicio15 {
+	public static void main(String[] args) {
+/*15.Pergunte para o usuário um dia do mês e número do mês.
+ *Faça um algoritmo que valide se o dia e o mês existem
+ *no calendário, utilizando apenas um Sysout.
+ *Sendo considerado duas variáveis int um para dia e
+ *outra para mês.*/		
+		Scanner scanner = new Scanner(System.in);
+			
+		scanner.close();
+	}
+}
