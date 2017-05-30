277aaec515601daa7201b36ad384c3340ccabc09
Noturno <Noturno@192.168.7.26>
2017-05-30 19:38:57 -0300

    Exercicio 3 feito

--- Algoritmos/src/Exercicios3/Exercicios3.java
+++ Algoritmos/src/Exercicios3/Exercicios3.java
@@ -1,13 +1,16 @@
 package Exercicios3;
 
-import java.io.StreamCorruptedException;
+
+
 import java.util.Scanner;
 
 public class Exercicios3 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-			double pizzas[] = new double[15];
-						
+		Scanner scanner = new Scanner(System.in);	
+		double pizzas[] = new double[15];
+		
+			
+			
 			System.out.println("Qual o valor da pizza de calabresa:");
 			pizzas[0] = scanner.nextDouble();
 			System.out.println("Qual o valor da pizza de 4 queijos:");
@@ -39,27 +42,80 @@ public class Exercicios3 {
 			System.out.println("Qual o valor da pizza de coco:");
 			pizzas[14] = scanner.nextDouble();
 			
+			
 			String cardapio[] = new String[15];
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de 4 queijos = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de frango = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de lombinho = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
-			cardapio[1] = "pizza de calabresa = " + Double.toString(pizzas[1]);
+			cardapio[0] = "pizza de calabresa = ";
+			cardapio[1] = "pizza de 4 queijos = ";
+			cardapio[2] = "pizza de frango = ";
+			cardapio[3] = "pizza de lombinho = ";
+			cardapio[4] = "pizza de coração = ";
+			cardapio[5] = "pizza de carne = ";
+			cardapio[6] = "pizza de strogonoff de frango = ";
+			cardapio[7] = "pizza de strogonoff de carne = ";
+			cardapio[8] = "pizza de 5 queijos = ";
+			cardapio[9] = "pizza de banana split = ";
+			cardapio[10] = "pizza de sonho de valsa = ";
+			cardapio[11] = "pizza de prestígio = ";
+			cardapio[12] = "pizza de chocolate preto = ";
+			cardapio[13] = "pizza de chocolate branco = ";
+			cardapio[14] = "pizza de coco = ";
 			
-			
-			
+						
 		for (int i = 0; i < pizzas.length; i++) {
 			System.out.println(cardapio[i] + pizzas[i]);
+		}
+		
+		int garcom;
+		System.out.println("Digite o numero da pizza:");
+		garcom = scanner.nextInt();
+		
+		switch (garcom) {
+		case 1:
+			System.out.println("O valor do seu pedido é de "+ pizzas[0]);
+		break;
+		case 2:
+			System.out.println("O valor do seu pedido é de "+ pizzas[1]);
+		break;
+		case 3:
+			System.out.println("O valor do seu pedido é de "+ pizzas[2]);
+		break;
+		case 4:
+			System.out.println("O valor do seu pedido é de "+ pizzas[3]);
+		break;
+		case 5:
+			System.out.println("O valor do seu pedido é de "+ pizzas[4]);
+		break;
+		case 6:
+			System.out.println("O valor do seu pedido é de "+ pizzas[5]);
+		break;
+		case 7:
+			System.out.println("O valor do seu pedido é de "+ pizzas[6]);
+		break;
+		case 8:
+			System.out.println("O valor do seu pedido é de "+ pizzas[7]);
+		break;
+		case 9:
+			System.out.println("O valor do seu pedido é de "+ pizzas[8]);
+		break;
+		case 10:
+			System.out.println("O valor do seu pedido é de "+ pizzas[9]);
+		break;
+		case 11:
+			System.out.println("O valor do seu pedido é de "+ pizzas[10]);
+		break;
+		case 12:
+			System.out.println("O valor do seu pedido é de "+ pizzas[11]);
+		break;
+		case 13:
+			System.out.println("O valor do seu pedido é de "+ pizzas[12]);
+		break;
+		case 14:
+			System.out.println("O valor do seu pedido é de "+ pizzas[13]);
+		break;
+		case 15:
+			System.out.println("O valor do seu pedido é de "+ pizzas[14]);
+		break;
+		default: System.out.println("Seu pedido não é valido");
 		}
 		scanner.close();
 	}
