7f9d3396747d62473d6b354ee689039329daa0ed
Noturno <Noturno@192.168.7.26>
2017-06-01 21:40:52 -0300

    ExercicioArray feito até 26

--- Algoritmos/src/ExercicioArray/ExercicioArray26.java
+++ Algoritmos/src/ExercicioArray/ExercicioArray26.java
@@ -1,17 +1,7 @@
 package ExercicioArray;
 
-
-public class ExercicioArray25 {
+public class ExercicioArray26 {
 	public static void main(String[] args) {
-/*25.Criar uma matriz com 3 linhas e 3 colunas.
- * Popular esta matriz da forma tradicional (matriz[0][0] = 1;)
- * com os valores desejados. Utilizando for, efetuar a soma dos valores
- * de cada linha e imprimir apenas os valores somados da linha.
- * Ou seja, conforme matriz abaixo:
-		3 3 3
-		2 2 2
-		1 1 1
-*/		
 		int[][] matriz = new int[3][3];
 		matriz[0][0] = 3;
 		matriz[0][1] = 3;
@@ -30,18 +20,16 @@ public class ExercicioArray25 {
 		
 		for (int linha = 0; linha < matriz.length; linha++) {
 			for (int coluna = 0; coluna < matriz.length; coluna++) {
-				somalinhas[linha] = somalinhas[linha] + matriz[linha][coluna];
-				
+				somalinhas[coluna] = somalinhas[coluna] + matriz[linha][coluna];
 			}
 		}
 		
 		for (int linha = 0; linha < somalinhas.length; linha++) {
 			for (int coluna = 0; coluna < somalinhas.length; coluna++) {
-				matriz[linha][coluna] = somalinhas[linha];
+				matriz[linha][coluna] = somalinhas[coluna];
 				if (coluna == matriz[coluna].length - 1) {
-					System.out.println("Soma da linha " + linha + ": " + somalinhas[linha]);
-					
-				}
+					System.out.println("Soma da coluna " + linha + ": " + somalinhas[coluna]);
+					}
 			}
 		}
 	}
