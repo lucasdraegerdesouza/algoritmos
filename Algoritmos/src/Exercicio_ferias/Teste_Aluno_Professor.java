ff5b29b2e45822d58cda47f6349f76166e690c84
lu_ca <lu_ca@DESKTOP-2BPVQ4U>
2017-07-12 12:44:55 -0300

    exercicio ferias

--- Algoritmos/src/Exercicio_ferias/Teste_Aluno_Professor.java
+++ Algoritmos/src/Exercicio_ferias/Teste_Aluno_Professor.java
@@ -8,10 +8,22 @@ public class Teste_Aluno_Professor {
 		al.setIdade(22);
 		al.setEndereco("Rua Carl Muller");
 		
-		Professor pf = new Professor();
-		//pf.setNome();
+		Professor pf = new Professor("professor_stefanie", "12345stefanie");
+		pf.setNome("Stefanie");
+		pf.setIdade(26);
+		pf.setEndereco("Rua Carl Muller");
 		
-		
+		System.out.println("Nome Aluno = "+al.getNome()+
+		"\nIdade = "+al.getIdade()+
+		"\nEndereço = "+al.getEndereco()+
+		"\nLogin = "+al.getLogin()+
+		"\nSenha = "+al.getSenha());
+		System.out.println("-------------------------------");
+		System.out.println("Nome Professora = "+pf.getNome()+
+		"\nIdade = "+pf.getIdade()+
+		"\nEndereço = "+pf.getEndereco()+
+		"\nLogin = "+pf.getLogin()+
+		"\nSenha ="+pf.getSenha());
 		
 	}
 }
