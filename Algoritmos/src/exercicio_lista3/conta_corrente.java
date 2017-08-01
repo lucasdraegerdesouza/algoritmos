ad7b3d35711cd5c57e025ee9f06eeff3fdf5a819
Noturno <Noturno@192.168.7.19>
2017-07-31 21:53:09 -0300

    conta corrente lista 3

---
+++ Algoritmos/src/exercicio_lista3/conta_corrente.java
@@ -0,0 +1,21 @@
+package exercicio_lista3;
+
+public class conta_corrente {
+	
+	private int n_conta;
+	private double saldo_cc;
+	
+	public int getN_conta() {
+		return n_conta;
+	}
+	public void setN_conta(int n_conta) {
+		this.n_conta = n_conta;
+	}
+	public double getSaldo_cc() {
+		return saldo_cc;
+	}
+	public void setSaldo_cc(double saldo_cc) {
+		this.saldo_cc = saldo_cc;
+	}
+
+}
